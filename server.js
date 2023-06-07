df4a914b81034da0c8cf96ff477a01fbf7d9647f
Nawal Jama <nawaljng@gmail.com>
2023-06-06 20:29:42 -0400

    C:/Program Files/Git/api/notes route is added

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+);
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
