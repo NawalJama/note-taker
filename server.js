bc5e4a25df641786187ea91e28c783920b4428bc
Nawal Jama <nawaljng@gmail.com>
2023-06-06 20:33:57 -0400

    added post on /api/notes

--- server.js
+++ server.js
@@ -18,6 +18,17 @@ app.get('/api/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/db/db.json'))
 );
 
+app.post("/api/notes", function (req, res) {
+    let notesInDbJson = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let reqNote = req.body;
+    let newID = notesInDbJson.length.toString();
+    reqNote.id = newID;
+    notesInDbJson.push(reqNote);
+  
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesInDbJson));
+    res.json(notesInDbJson);
+  });  
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
